05196456ccd98b64745f8d7f65d4108d973764f6
waldomessiasprofessor-ui <waldomessiasprofessor@gmail.com>
2026-03-29 11:43:33 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
 import { Toaster } from "sonner";
-import { ThemeProvider } from "next-themes";
 import App from "./App";
 import { trpc } from "./lib/trpc";
 import "../index.css";
@@ -14,10 +13,10 @@ const queryClient = new QueryClient({
 });
 
 const trpcClient = trpc.createClient({
-  transformer: superjson,
   links: [
     httpBatchLink({
       url: "/api/trpc",
+      transformer: superjson,
       fetch: (url, options) =>
         fetch(url, { ...options, credentials: "include" }),
     }),
@@ -28,10 +27,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <App />
-          <Toaster richColors position="top-right" />
-        </ThemeProvider>
+        <App />
+        <Toaster richColors position="top-right" />
       </QueryClientProvider>
     </trpc.Provider>
   </React.StrictMode>
